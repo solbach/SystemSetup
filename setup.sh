4743159cee58a0d22c611ae7af92ed98fed46c84
solbach <solbach@eecs.yorku.ca>
2017-01-19 13:18:29 -0500

    Updated and upgradet version for Ubuntu 16.04

--- setup.sh
+++ setup.sh
@@ -1,31 +1,33 @@
 # Ubuntu Stuff
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-sudo add-apt-repository -y ppa:alexey-ivanov/qtcreator
-sudo apt-add-repository -y ppa:indicator-multiload/stable-daily
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo add-apt-repository -y ppa:webupd8team/java
+#sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
 sudo apt-get update
+#sudo apt-get install -y nvidia-370
+sudo apt-get install -y --allow-unauthenticated owncloud-client
 sudo apt-get install -y indicator-multiload
+sudo apt-get install -y unity-tweak-tool
+sudo apt-get install -y compizconfig-settings-manager
 sudo apt-get install -y sublime-text-installer
-sudo apt-get install -y libglew-dev libcheese7 libcheese-gtk23 libclutter-gst-2.0-0 libcogl15 libclutter-gtk-1.0-0 libclutter-1.0-0  xserver-xorg-input-all
-sudo apt-get install -y qtcreator
 sudo apt-get install -y vim
 sudo apt-get install -y imagemagick
 sudo apt-get install -y htop
+sudo apt-get install -y system-config-lvm
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y oracle-java8-set-default
 
-# ROS Indigo
+# ROS Kinetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install -y ros-indigo-desktop-full
+sudo apt-get install -y ros-kinetic-desktop-full
 
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-source /opt/ros/indigo/setup.bash
 sudo apt-get install -y python-rosinstall
