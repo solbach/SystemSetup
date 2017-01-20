922d9c4bdd24f1b9e725e04bc3656a906f56b547
solbach <solbach@eecs.yorku.ca>
2017-01-20 14:09:47 -0500

    vlc added

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ sudo apt-get install -y vim
 sudo apt-get install -y smbclient
 sudo apt-get install -y imagemagick
 sudo apt-get install -y htop
+sudo apt-get install -y vlc
 sudo apt-get install -y system-config-lvm
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
