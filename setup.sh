c8d76b186edca89e7c5261e80409a852283dca82
solbach <solbach@eecs.yorku.ca>
2017-02-14 14:25:21 -0500

    added latex and beamer support. also installs impressive to present beamer slides

--- setup.sh
+++ setup.sh
@@ -5,13 +5,20 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo add-apt-repository -y ppa:thomas-schiex/blender
+sudo add-apt-repository -y ppa:texlive-backports/ppa
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud-client.list"
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 sudo dpkg --add-architecture i386
 sudo apt-get update
+sudo apt-get install -y texlive-full 
+sudo apt-get install -y biblatex
+sudo apt-get install -y latex-beamer
+sudo apt-get install -y impressive
 sudo apt-get install -y skype
 sudo apt-get install -y okular
+sudo apt-get install -y samba
 sudo apt-get install -y blender
+sudo apt-get install -y openvpn
 sudo apt-get install -y nvidia-370
 sudo apt-get install -y --allow-unauthenticated owncloud-client
 sudo apt-get install -y indicator-multiload
